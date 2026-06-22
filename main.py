0322b7961acc7548b717c6430dffb1323759896c
pseudowizardry <AmadeusJoelAJ@Gmail.com>
2026-06-22 10:02:57 +0800

    C: add shooting mechanic

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from logger import log_state
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
+import sys
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,15 +15,42 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    
+
     while True:
         log_state()
 
         for event in pygame.event.get():
-            pass    
-
+            if event.type == pygame.QUIT:   
+                return   
+            
+        dt = clock.tick(60) / 1000 
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
-
 
 if __name__ == "__main__":
     main()
